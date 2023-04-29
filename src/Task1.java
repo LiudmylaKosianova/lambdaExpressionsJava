688cb59e386fd05d009b9a96682b8df2aff0a53b
Blueberry <snw.wht.lk@gmail.com>
2023-04-29 10:12:40 +0200

    it works so far

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -7,13 +8,18 @@ public class Task1 {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
-
-
+        //print without lambda
         for(Integer number : numbers){
             System.out.println(number);
         }
-
+        //print with lambda
         numbers.forEach( (number) -> System.out.println("with lambda: " + number));
+        //replace with random and print with lambda
+        Random random = new Random();
+        numbers.forEach( (element) -> {
+            element = random.nextInt(11);
+            System.out.println("replaced with random: "+ element);
+        }  );
 
     }
 
