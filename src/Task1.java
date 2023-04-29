578dc9300798b3aa29506d5e20ee8368c39b47ae
Blueberry <snw.wht.lk@gmail.com>
2023-04-29 10:03:39 +0200

    it works so far

--- src/Task1.java
+++ src/Task1.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+
+        for(Integer number : numbers){
+            System.out.println(number);
+        }
+
+        numbers.forEach( (number) -> System.out.println("with lambda: " + number));
+
+    }
 
 }
